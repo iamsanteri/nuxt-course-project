daf6613b515276de9a60878a18a714a0e6258a2b
Santeri Liukkonen <santeri@santeris-mbp.localdomain>
2020-02-09 16:19:11 +0100

    Initial commit

---
+++ components/posts/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="post-list">
+        <PostPreview :is-admin="isAdmin" id="1" thumbnail="-" title="Hello there!" previewText="This is a preview text." />
+        <PostPreview :is-admin="isAdmin" id="2" thumbnail="-" title="Hello there! 2" previewText="This is a preview text. 2" />
+        <PostPreview :is-admin="isAdmin" id="3" thumbnail="-" title="Hello there! 3" previewText="This is a preview text. 3" />
+    </section>
+</template>
+
+<script>
+import PostPreview from "@/components/posts/PostPreview"
+
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+      isAdmin: {
+          type: Boolean, 
+          default: false
+      }
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+<script>
+import AdminPostForm from "@/components/admin/AdminPostForm"
+
+export default {
+    layout: "admin",
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: "Santeri",
+                title: "My Awesome Post", 
+                content: "Super amazing, thanks for that!",
+                thumbnail: "-"
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
