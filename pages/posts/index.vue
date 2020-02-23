e7001c6046f6747080f0fe4fde32e23996a08c29
Santeri Liukkonen <santeri@santeris-mbp.localdomain>
2020-02-23 20:31:35 +0100

    Added and initialized a Vuex store

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="posts-page">
-        <PostList />
+        <PostList :posts="loadedPosts" />
     </div>
 </template>
 
@@ -10,6 +10,17 @@ import PostList from "@/components/posts/PostList"
 export default {
   components: {
     PostList
+  },
+  data() {
+    return {
+      loadedPosts: [
+        { id: "1", title: "First post", previewText: "This is our first post", thumbnail: "-"},
+        { id: "2", title: "Second post", previewText: "This is our second post", thumbnail: "-"}
+      ]
+    }
+  },
+  created() {
+    this.$store.dispatch("setPosts", this.loadedPosts);
   }
 }
 </script>
